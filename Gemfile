1f8dd17c4353b14de23319b9de81d508b45462a0
uu59 <k@uu59.org>
2014-05-13 16:23:37 +0900

    model spec

--- Gemfile
+++ Gemfile
@@ -8,4 +8,5 @@ group :development, :test do
   gem "rake"
   gem "pry"
   gem "rspec-rails", "~> 2.0"
+  gem "factory_girl_rails"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  validates :name, uniqueness: true, presence: true
+  validates :remember_token, uniqueness: true, allow_nil: true
+
   def generate_remember_token
     begin
       token = SecureRandom.base64(32)
