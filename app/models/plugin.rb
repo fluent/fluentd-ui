058f4ab122e5233c907e884ff75443a26c53e273
uu59 <k@uu59.org>
2014-05-16 11:45:37 +0900

    Refactoring

--- app/models/plugin.rb
+++ app/models/plugin.rb
@@ -16,38 +16,37 @@ class Plugin
   end
 
   def install!
-    unless installed?
-      self.version = latest_version unless version
-      if valid?
-        if fluent_gem("install", gem_name, "-v", version)
-          File.open(gemfile_path, "a") do |f|
-            f.puts format_gemfile
-          end
-        end
+    return if installed?
+
+    self.version ||= latest_version
+    if valid? && gem_install
+      File.open(gemfile_path, "a") do |f|
+        f.puts format_gemfile
       end
     end
   end
 
   def uninstall!
-    if installed?
-      # NOTE: do not uninstall gem actually for now. because it is not necessary, and slow job
-      # NOTE: should uninstall that situation: installed verions is A, self.version is NOT A.
-      new_gemfile = ""
-      File.open(gemfile_path).each_line do |line|
-        next if line.include?(%Q|gem "#{gem_name}"|)
-        new_gemfile << line
-      end
-      File.open(gemfile_path, "w"){|f| f.write new_gemfile }
+    return unless installed?
+
+    # NOTE: do not uninstall gem actually for now. because it is not necessary, and slow job
+    # NOTE: should uninstall that situation: installed verions is A, self.version is NOT A. only check gem_name.
+    new_gemfile = ""
+    File.open(gemfile_path).each_line do |line|
+      next if line.include?(%Q|gem "#{gem_name}"|)
+      new_gemfile << line
     end
+    File.open(gemfile_path, "w"){|f| f.write new_gemfile }
   end
 
   def upgrade!(new_version)
-    if installed?
-      upgrade = self.class.new(gem_name: self.gem_name, version: new_version)
-      if self.valid? && upgrade.valid?
-        self.uninstall!
-        upgrade.install!
-      end
+    return unless installed?
+
+    upgrade = self.class.new(gem_name: self.gem_name, version: new_version)
+    if self.valid? && upgrade.valid?
+      self.uninstall!
+      upgrade.install!
+      self.version = upgrade.version
     end
   end
 
@@ -58,7 +57,7 @@ class Plugin
   end
 
   def format_gemfile
-    self.version = latest_version unless version
+    self.version ||= latest_version
     %Q|gem "#{gem_name}", "#{version}"|
   end
 
@@ -85,7 +84,7 @@ class Plugin
   end
 
   def self.gemfile_changed?
-    # if true, rails server needs to restart }
+    # if true, rails server needs to restart. new installed/removed gems are.
     @initial_gemfile_content != File.read(gemfile_path)
   end
 
@@ -115,7 +114,12 @@ class Plugin
 
   private
 
+  def gem_install
+    fluent_gem("install", gem_name, "-v", version)
+  end
+
   def fluent_gem(*commands)
+    # NOTE: use `fluent-gem` instead of `gem`
     unless system(*%W(bundle exec fluent-gem) + commands) # TODO: should grab stdout/stderr
       raise GemError, "failed command #{commands.join(" ")}"
     end
