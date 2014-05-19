0c39deaa7160b078e9a9ac339f0464cd67c25a98
uu59 <k@uu59.org>
2014-05-19 18:30:23 +0900

    Fix to not install plugin if it is installing

--- app/workers/gem_installer.rb
+++ app/workers/gem_installer.rb
@@ -7,10 +7,12 @@ class GemInstaller
   def perform(gem_name, version = nil)
     SuckerPunch.logger.info "install #{gem_name} #{version}"
     pl = Plugin.new(gem_name: gem_name, version: version)
-    WORKING.push(pl)
-    pl.uninstall! if pl.installed?
-    pl.install!
-    WORKING.delete(pl)
+    unless WORKING.find{|p| p.gem_name == pl.gem_name}
+      WORKING.push(pl)
+      pl.uninstall! if pl.installed?
+      pl.install!
+      WORKING.delete(pl)
+    end
     SuckerPunch.logger.info "installed #{gem_name} #{version}"
   end
 end
