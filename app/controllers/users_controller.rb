6fbd1c6bfe5afbdb5a5c89af752ffc0a2b6f630f
uu59 <k@uu59.org>
2014-05-20 14:34:02 +0900

    Add /misc/users, information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,7 @@
 class UsersController < ApplicationController
   before_action :login_required
+
+  def index
+    @users = User.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,10 @@ Rails.application.routes.draw do
   end
 
 
-  resources :misc, only: [] do
+  resource :misc do
+    get "information"
+    resources :users do
+    end
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
