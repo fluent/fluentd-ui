76fc011222deb33abf22548074d7064deb6131a6
uu59 <k@uu59.org>
2014-05-26 16:57:17 +0900

    Move recommended plugins to model from controller (still TODO)

--- app/models/plugin.rb
+++ app/models/plugin.rb
@@ -92,6 +92,56 @@ class Plugin
     end
   end
 
+  def self.recommended
+    # TODO: how to manage recommended plugins?
+    [
+      Plugin.new(gem_name: "fluent-plugin-mongo"),
+      Plugin.new(gem_name: "fluent-plugin-s3"),
+      Plugin.new(gem_name: "fluent-plugin-secure-forward"),
+      Plugin.new(gem_name: "fluent-plugin-forest"),
+      Plugin.new(gem_name: "fluent-plugin-couch"),
+      Plugin.new(gem_name: "fluent-plugin-dstat"),
+      Plugin.new(gem_name: "fluent-plugin-parser"),
+      Plugin.new(gem_name: "fluent-plugin-map"),
+      Plugin.new(gem_name: "fluent-plugin-grep"),
+      Plugin.new(gem_name: "fluent-plugin-webhdfs"),
+      Plugin.new(gem_name: "fluent-plugin-rewrite-tag-filter"),
+      Plugin.new(gem_name: "fluent-plugin-td"),
+      Plugin.new(gem_name: "fluent-plugin-elasticsearch"),
+      Plugin.new(gem_name: "fluent-plugin-datacounter"),
+      Plugin.new(gem_name: "fluent-plugin-flowcounter"),
+      Plugin.new(gem_name: "fluent-plugin-numeric-counter"),
+      Plugin.new(gem_name: "fluent-plugin-record-reformer"),
+      Plugin.new(gem_name: "fluent-plugin-pghstore"),
+      Plugin.new(gem_name: "fluent-plugin-notifier"),
+      Plugin.new(gem_name: "fluent-plugin-mysqlslowquery"),
+      Plugin.new(gem_name: "fluent-plugin-boundio"),
+      Plugin.new(gem_name: "fluent-plugin-ses"),
+      Plugin.new(gem_name: "fluent-plugin-groonga"),
+      Plugin.new(gem_name: "fluent-plugin-websocket"),
+      Plugin.new(gem_name: "fluent-plugin-pgjson"),
+      Plugin.new(gem_name: "fluent-plugin-say"),
+      Plugin.new(gem_name: "fluent-plugin-uri_decoder"),
+      Plugin.new(gem_name: "fluent-plugin-rds-log"),
+      Plugin.new(gem_name: "fluent-plugin-msgpack-rpc"),
+      Plugin.new(gem_name: "fluent-plugin-reemit"),
+      Plugin.new(gem_name: "fluent-plugin-multiprocess"),
+      Plugin.new(gem_name: "fluent-plugin-dbi"),
+      Plugin.new(gem_name: "fluent-plugin-serialport"),
+      Plugin.new(gem_name: "fluent-plugin-loggly"),
+      Plugin.new(gem_name: "fluent-plugin-out-solr"),
+      Plugin.new(gem_name: "fluent-plugin-riak"),
+      Plugin.new(gem_name: "fluent-plugin-couchbase"),
+      Plugin.new(gem_name: "fluent-plugin-unique-counter"),
+      Plugin.new(gem_name: "fluent-plugin-flatten-hash"),
+      Plugin.new(gem_name: "fluent-plugin-solr"),
+      Plugin.new(gem_name: "fluent-plugin-dd"),
+      Plugin.new(gem_name: "fluent-plugin-gstore"),
+      Plugin.new(gem_name: "fluent-plugin-mongokpi"),
+      Plugin.new(gem_name: "fluent-plugin-jsonbucket"),
+    ]
+  end
+
   def self.gemfile_path
     Rails.root + "Gemfile.plugins"
   end
