d60c2e09898fca60def8366dad98a2db4be597e9
uu59 <k@uu59.org>
2014-05-26 16:39:47 +0900

    Set default API endpoint for fluentd

--- app/controllers/fluentd_controller.rb
+++ app/controllers/fluentd_controller.rb
@@ -6,7 +6,7 @@ class FluentdController < ApplicationController
   end
 
   def new
-    @fluentd = Fluentd.new(Fluentd::Agent::Fluentd.default_options) # TODO: not fluentd type
+    @fluentd = Fluentd.new(Fluentd::Agent::Fluentd.default_options.merge(:api_endpoint => "http://localhost:24220/")) # TODO: not fluentd type
   end
 
   def create
