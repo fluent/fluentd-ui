0273a8dbe0c15164addd4af3633f7274d619c47a
uu59 <k@uu59.org>
2014-06-04 15:26:26 +0900

    Check to be ready for start tutorial

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,6 +1,14 @@
 class TutorialsController < ApplicationController
+  before_action :find_fluentd
 
   def index
+    @log = @fluentd.agent.log_tail.reverse if @fluentd
   end
 
+  private
+
+  def find_fluentd
+    # NOTE: use first fluentd for tutorial
+    @fluentd = Fluentd.first
+  end
 end
---
+++ spec/controllers/tutorials_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe TutorialsController, :type => :controller do
+
+end
