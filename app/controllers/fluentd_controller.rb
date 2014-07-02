35346045a972ce9d2a627a770d3acf32ddd4803c
uu59 <k@uu59.org>
2014-07-02 13:04:18 +0900

    Can delete fluentd setting

--- app/controllers/fluentd_controller.rb
+++ app/controllers/fluentd_controller.rb
@@ -33,7 +33,7 @@ class FluentdController < ApplicationController
   def destroy
     @fluentd.agent.stop if @fluentd.agent.running?
     @fluentd.destroy
-    redirect_to root_path
+    redirect_to root_path, flash: {success: t('messages.destroy_succeed_fluentd_setting')}
   end
 
   def log
