6e87aceef3cd4a2375b7e6f9c6de785f1f226443
uu59 <k@uu59.org>
2014-07-02 15:58:21 +0900

    Robust plugin install/uninstall proccess

--- app/workers/gem_installer.rb
+++ app/workers/gem_installer.rb
@@ -5,8 +5,13 @@ class GemInstaller
   def perform(gem_name, version = nil)
     SuckerPunch.logger.info "install #{gem_name} #{version}"
     pl = Plugin.new(gem_name: gem_name, version: version)
-    pl.uninstall! if pl.installed?
-    pl.install!
-    SuckerPunch.logger.info "installed #{gem_name} #{version}"
+    begin
+      # NOTE: uninstall all versions of `gem_name` then install it for upgrade/downgrade
+      pl.uninstall! if pl.installed?
+      pl.install!
+      SuckerPunch.logger.info "installed #{gem_name} #{version}"
+    rescue Plugin::GemError
+      SuckerPunch.logger.warn "installing #{gem_name} #{version} is failed"
+    end
   end
 end
--- app/workers/gem_uninstaller.rb
+++ app/workers/gem_uninstaller.rb
@@ -5,7 +5,11 @@ class GemUninstaller
   def perform(gem_name)
     SuckerPunch.logger.info "uninstall #{gem_name}"
     pl = Plugin.new(gem_name: gem_name)
-    pl.uninstall!
-    SuckerPunch.logger.info "uninstalled #{gem_name}"
+    begin
+      pl.uninstall!
+      SuckerPunch.logger.info "uninstalled #{gem_name}"
+    rescue Plugin::GemError
+      SuckerPunch.logger.warn "uninstalling #{gem_name} is failed"
+    end
   end
 end
