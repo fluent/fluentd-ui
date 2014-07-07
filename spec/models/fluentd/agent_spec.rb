d1ad601bbf0f344d81eca1f1be69b10d3cff0cf7
uu59 <k@uu59.org>
2014-07-07 16:39:52 +0900

    Fix spec for restart strategy changed

--- spec/models/fluentd/agent_spec.rb
+++ spec/models/fluentd/agent_spec.rb
@@ -192,36 +192,95 @@ describe Fluentd::Agent do
     end
 
     describe "#restart" do
-      before { instance.stub(:stop).and_return(stop_result) }
-      before { instance.stub(:start).and_return(start_result) }
+      before { instance.stub(:running?).and_return(running) }
       subject { instance.restart }
 
-      describe "return true only if #stop and #start success" do
-        context "#stop success" do
-          let(:stop_result) { true } 
+      context "not running" do
+        before { instance.stub(:start).and_return(start) }
 
-          context" #start success" do
-            let(:start_result) { true } 
-            it { should be_truthy }
-          end
+        let(:running) { false }
 
-          context" #start fail" do
-            let(:start_result) { false } 
-            it { should be_falsy }
-          end
+        context "#start success" do
+          let(:start) { true }
+          it { should be_truthy }
         end
 
-        context "#stop fail" do
-          let(:stop_result) { false } 
+        context "#start failed" do
+          let(:start) { false }
+          it { should be_falsy }
+        end
+      end
 
-          context" #start success" do
-            let(:start_result) { true } 
-            it { should be_falsy }
+      context "running" do
+        before { instance.stub(:stop).and_return(stop_result) }
+        before { instance.stub(:start).and_return(start_result) }
+        before { instance.stub(:validate_fluentd_options).and_return(validate_result) }
+
+        let(:running) { true }
+
+        describe "return true only if #stop and #start success" do
+          context "#validate_fluentd_options success" do
+            let(:validate_result) { true }
+
+            context "#stop success" do
+              let(:stop_result) { true }
+
+              context" #start success" do
+                let(:start_result) { true }
+                it { should be_truthy }
+              end
+
+              context" #start fail" do
+                let(:start_result) { false }
+                it { should be_falsy }
+              end
+            end
+
+            context "#stop fail" do
+              let(:stop_result) { false }
+
+              context" #start success" do
+                let(:start_result) { true }
+                it { should be_falsy }
+              end
+
+              context" #start fail" do
+                let(:start_result) { false }
+                it { should be_falsy }
+              end
+            end
           end
 
-          context" #start fail" do
-            let(:start_result) { false } 
-            it { should be_falsy }
+          context "#validate_fluentd_options failed" do
+            let(:validate_result) { false }
+
+            context "#stop success" do
+              let(:stop_result) { true }
+
+              context" #start success" do
+                let(:start_result) { true } 
+                it { should be_falsy }
+              end
+
+              context" #start fail" do
+                let(:start_result) { false } 
+                it { should be_falsy }
+              end
+            end
+
+            context "#stop fail" do
+              let(:stop_result) { false } 
+
+              context" #start success" do
+                let(:start_result) { true } 
+                it { should be_falsy }
+              end
+
+              context" #start fail" do
+                let(:start_result) { false } 
+                it { should be_falsy }
+              end
+            end
           end
         end
       end
