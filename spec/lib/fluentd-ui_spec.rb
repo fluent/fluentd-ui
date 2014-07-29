8cfc2a3d43391cb8043c5f14dcbe95f7febe66e7
uu59 <k@uu59.org>
2014-07-29 14:58:27 +0900

    Add spec for lib/

--- spec/lib/fluentd-ui_spec.rb
+++ spec/lib/fluentd-ui_spec.rb
@@ -16,4 +16,20 @@ describe FluentdUI do
       it { should be_truthy }
     end
   end
+
+  describe ".fluentd_version" do
+    before { Fluentd.stub(:instance).and_return(target) }
+    subject { FluentdUI.fluentd_version }
+
+    context "not setup yet" do
+      let(:target) { nil }
+      it { should be_nil }
+    end
+
+    context "did setup" do
+      let(:target) { build(:fluentd) }
+      let(:version) { "1.1.1" }
+      it { should == target.agent.version }
+    end
+  end
 end
