e79ebf7da01b2c5ee2c6f9b6c9a515f4eff70d70
uu59 <k@uu59.org>
2014-11-06 14:29:31 +0900

    Add specs for newer fluentd-ui available check and popup

--- config/initializers/fluentd_ui_update_check.rb
+++ config/initializers/fluentd_ui_update_check.rb
@@ -1,4 +1,6 @@
-unless FluentdUI.td_agent_ui?
-  # td-agent-ui shouldn't auto update
-  FluentdUiUpdateCheck.new.async.perform
+unless Rails.env.test?
+  unless FluentdUI.td_agent_ui?
+    # td-agent-ui shouldn't auto update
+    FluentdUiUpdateCheck.new.async.perform
+  end
 end
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,6 +1,6 @@
-describe "users" do
-  let(:exists_user) { create(:user) }
+require "spec_helper"
 
+describe "users" do
   describe "edit" do
     let(:url) { user_path }
     it_should_behave_like "login required"
