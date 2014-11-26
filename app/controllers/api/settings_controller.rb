eb465221b86bcd525578a95e36c748b989dddab1
uu59 <k@uu59.org>
2014-11-26 15:44:11 +0900

    Error handling

--- app/controllers/api/settings_controller.rb
+++ app/controllers/api/settings_controller.rb
@@ -13,12 +13,20 @@ class Api::SettingsController < ApplicationController
     coming = Fluent::Config::V1Parser.parse(params[:content], @fluentd.config_file)
     current = @section
     index = @config.elements.index current
+    unless index
+      render_404
+      return
+    end
     @config.elements[index] = coming.elements.first
     @config.write_to_file
     redirect_to api_setting_path(id: element_id(coming.elements.first))
   end
 
   def destroy
+    unless @config.elements.index(@section)
+      render_404
+      return
+    end
     @config.elements.delete @section
     @config.write_to_file
     head :no_content # 204
@@ -27,19 +35,21 @@ class Api::SettingsController < ApplicationController
   private
 
   def set_config
-    # TODO: not found
     @config = Fluentd::Setting::Config.new(@fluentd.config_file)
   end
 
   def set_section
-    # TODO: not found
     @section = @config.elements.find do |elm|
       element_id(elm) == params[:id]
     end
   end
 
   def element_id(element)
-    index = @config.elements.index(element) # TODO: not found
+    index = @config.elements.index(element)
     "#{"%06d" % index}#{Digest::MD5.hexdigest(element.to_s)}"
+  end
+
+  def render_404
+    render nothing: true, status: 404
   end
 end
