3be2bcb226d79919b8e9d764856cd75585f4fc9a
hassaku <hassaku.apps@gmail.com>
2014-12-03 15:30:31 +0900

    Refactor feature specs for settings.

--- spec/features/fluentd/setting/in_monitor_agent_spec.rb
+++ spec/features/fluentd/setting/in_monitor_agent_spec.rb
@@ -1,22 +1,9 @@
 require "spec_helper"
 
-describe "in_monitor_agent" do
-  let(:exists_user) { build(:user) }
-  let(:daemon) { build(:fluentd, variant: "td-agent") }
+describe "in_monitor_agent", stub: :daemon do
   let(:port) { "12345" }
 
-  before do
-    Fluentd.stub(:instance).and_return(daemon)
-    Fluentd::Agent::TdAgent.any_instance.stub(:detached_command).and_return(true)
-    daemon.agent.config_write ""
-    
-    visit '/sessions/new'
-    within("form") do
-      fill_in 'session_name', :with => exists_user.name
-      fill_in 'session_password', :with => exists_user.password
-    end
-    click_button I18n.t("terms.sign_in")
-  end
+  before { login_with exists_user }
 
   it "Shown form with filled in td.*.* on match" do
     visit daemon_setting_in_monitor_agent_path
--- spec/features/fluentd/setting/out_stdout_spec.rb
+++ spec/features/fluentd/setting/out_stdout_spec.rb
@@ -1,22 +1,9 @@
 require "spec_helper"
 
-describe "out_stdout" do
-  let(:exists_user) { build(:user) }
-  let(:daemon) { build(:fluentd, variant: "td-agent") }
+describe "out_stdout", stub: :daemon do
   let(:match) { "stdout.**" }
 
-  before do
-    Fluentd.stub(:instance).and_return(daemon)
-    Fluentd::Agent::TdAgent.any_instance.stub(:detached_command).and_return(true)
-    daemon.agent.config_write ""
-    
-    visit '/sessions/new'
-    within("form") do
-      fill_in 'session_name', :with => exists_user.name
-      fill_in 'session_password', :with => exists_user.password
-    end
-    click_button I18n.t("terms.sign_in")
-  end
+  before { login_with exists_user }
 
   it "Shown form with filled in td.*.* on match" do
     visit daemon_setting_out_stdout_path
