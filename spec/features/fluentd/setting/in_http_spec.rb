c1a35ffbf41149f6a428355d155e3fcb19065bcf
hassaku <hassaku.apps@gmail.com>
2014-12-03 13:37:27 +0900

    Add feature spec for in_http setting.

---
+++ spec/features/fluentd/setting/in_http_spec.rb
@@ -0,0 +1,36 @@
+require "spec_helper"
+
+describe "in_http" do
+  let(:exists_user) { build(:user) }
+  let(:daemon) { build(:fluentd, variant: "td-agent") }
+  let(:port) { "12345" }
+
+  #TODO: wanna DRY with shared_context
+  before do
+    Fluentd.stub(:instance).and_return(daemon)
+    Fluentd::Agent::TdAgent.any_instance.stub(:detached_command).and_return(true)
+    daemon.agent.config_write ""
+
+    visit '/sessions/new'
+    within("form") do
+      fill_in 'session_name', :with => exists_user.name
+      fill_in 'session_password', :with => exists_user.password
+    end
+    click_button I18n.t("terms.sign_in")
+  end
+
+  it "Shown form with filled in td.*.* on match" do
+    visit daemon_setting_in_http_path
+    page.should have_css('input[name="fluentd_setting_in_http[port]"]')
+  end
+
+  it "Updated config after submit" do
+    daemon.agent.config.should_not include(port)
+    visit daemon_setting_in_http_path
+    within('#new_fluentd_setting_in_http') do
+      fill_in "Port", with: port
+    end
+    click_button I18n.t("fluentd.common.finish")
+    daemon.agent.config.should include(port)
+  end
+end
