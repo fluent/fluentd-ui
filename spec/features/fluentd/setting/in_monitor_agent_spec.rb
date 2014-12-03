4c61cc45f01e2797c6a5d705982d6c641abf0d42
hassaku <hassaku.apps@gmail.com>
2014-12-03 17:16:18 +0900

    Refactor feature specs for daemon settings.

--- spec/features/fluentd/setting/in_monitor_agent_spec.rb
+++ spec/features/fluentd/setting/in_monitor_agent_spec.rb
@@ -1,22 +1,7 @@
 require "spec_helper"
 
 describe "in_monitor_agent", stub: :daemon do
-  let(:port) { "12345" }
-
   before { login_with exists_user }
+  it_should_behave_like "configurable daemon settings", "in_monitor_agent", "port", "12345"
 
-  it "Shown form with filled in td.*.* on match" do
-    visit daemon_setting_in_monitor_agent_path
-    page.should have_css('input[name="fluentd_setting_in_monitor_agent[port]"]')
-  end
-
-  it "Updated config after submit" do
-    daemon.agent.config.should_not include(port)
-    visit daemon_setting_in_monitor_agent_path
-    within('#new_fluentd_setting_in_monitor_agent') do
-      fill_in "Port", with: port
-    end
-    click_button I18n.t("fluentd.common.finish")
-    daemon.agent.config.should include(port)
-  end
 end
--- spec/features/fluentd/setting/out_stdout_spec.rb
+++ spec/features/fluentd/setting/out_stdout_spec.rb
@@ -1,22 +1,7 @@
 require "spec_helper"
 
 describe "out_stdout", stub: :daemon do
-  let(:match) { "stdout.**" }
-
   before { login_with exists_user }
+  it_should_behave_like "configurable daemon settings", "out_stdout", "match", "stdout.**"
 
-  it "Shown form with filled in td.*.* on match" do
-    visit daemon_setting_out_stdout_path
-    page.should have_css('input[name="fluentd_setting_out_stdout[match]"]')
-  end
-
-  it "Updated config after submit" do
-    daemon.agent.config.should_not include(match)
-    visit daemon_setting_out_stdout_path
-    within('#new_fluentd_setting_out_stdout') do
-      fill_in "Match", with: match
-    end
-    click_button I18n.t("fluentd.common.finish")
-    daemon.agent.config.should include(match)
-  end
 end
