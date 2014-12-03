6bf8edcec798e0dbb43d8524fe7b96544f2012bc
Koichi TANAKA <tanaka51.jp@gmail.com>
2014-12-03 15:15:25 +0900

    Use capybara-screenshot on rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,7 @@ require 'webmock/rspec'
 WebMock.disable_net_connect!(allow_localhost: true)
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
+require 'capybara-screenshot/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
