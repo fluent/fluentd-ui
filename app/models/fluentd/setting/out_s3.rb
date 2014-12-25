51df93d3de9a47ca492a5f1fbfb6eaf9dd452dec
uu59 <k@uu59.org>
2014-12-25 16:42:06 +0900

    Support out_s3 v0.5.x

--- app/models/fluentd/setting/out_s3.rb
+++ app/models/fluentd/setting/out_s3.rb
@@ -4,7 +4,12 @@ class Fluentd
       include ActiveModel::Model
       include Common
 
-      configure_with_yaml "out_s3-0.4.x.yml"
+      pl = Plugin.new(gem_name: "fluent-plugin-s3")
+      if !pl.installed_version || pl.installed_version >= "0.5.0"
+        configure_with_yaml "out_s3-0.5.x.yml"
+      else
+        configure_with_yaml "out_s3-0.4.x.yml"
+      end
     end
   end
 end
