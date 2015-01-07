a28b71084c00c92d3c06516d17c75d07376683df
hassaku <hassaku.apps@gmail.com>
2015-01-07 12:06:44 +0900

    Associate a note with a backup file.

--- app/models/fluentd/setting_archive/backup_file.rb
+++ app/models/fluentd/setting_archive/backup_file.rb
@@ -2,15 +2,18 @@ class Fluentd
   module SettingArchive
     class BackupFile
       include Archivable
+      attr_reader :note
 
       FILE_EXTENSION = ".conf".freeze
 
       def self.find_by_file_id(backup_dir, file_id)
-        new(file_path_of(backup_dir, file_id))
+        note = Note.find_by_file_id(backup_dir, file_id) rescue nil
+        new(file_path_of(backup_dir, file_id), note)
       end
 
-      def initialize(file_path)
+      def initialize(file_path, note = nil)
         @file_path = file_path
+        @note = note || Note.new(file_path.sub(/#{FILE_EXTENSION}$/, Note::FILE_EXTENSION))
       end
     end
   end
