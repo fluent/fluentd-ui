6572b990bbb329ef2a21fe1c5482bd65cc4f7ff6
Ryan T. Hosford <tad.hosford@gmail.com>
2015-01-13 00:28:02 -0600

    Addresses duplicate code in agents_controller
      - Adds agents_controller_spec

--- app/controllers/fluentd/agents_controller.rb
+++ app/controllers/fluentd/agents_controller.rb
@@ -2,34 +2,32 @@ class Fluentd::AgentsController < ApplicationController
   before_action :find_fluentd
 
   def start
-    if @fluentd.agent.start
-      flash[:success] = t("messages.fluentd_start_stop_delay_notice", action: t('fluentd.common.start'))
-    else
-      flash[:error] = t("messages.fluentd_start_failed", brand: fluentd_ui_title) + @fluentd.agent.log_tail(1).first
-    end
+    run_action(__method__) { @fluentd.agent.log_tail(1).first }
     redirect_to daemon_path(@fluentd), status: 303 # 303 is change HTTP Verb GET
   end
 
   def stop
-    if @fluentd.agent.stop
-      flash[:success] = t("messages.fluentd_start_stop_delay_notice", action: t('fluentd.common.stop'))
-    else
-      flash[:error] = t("messages.fluentd_stop_failed", brand: fluentd_ui_title)
-    end
+    run_action(__method__)
     redirect_to daemon_path(@fluentd), status: 303 # 303 is change HTTP Verb GET
   end
 
   def restart
-    if @fluentd.agent.restart
-      flash[:success] = t("messages.fluentd_start_stop_delay_notice", action: t('fluentd.common.restart'))
-    else
-      flash[:error] = t("messages.fluentd_restart_failed", brand: fluentd_ui_title) + @fluentd.agent.log_tail(1).first
-    end
+    run_action(__method__) { @fluentd.agent.log_tail(1).first }
     redirect_to daemon_path(@fluentd), status: 303 # 303 is change HTTP Verb GET
   end
 
   def log_tail
     @logs = @fluentd.agent.log_tail(params[:limit]).reverse if @fluentd
     render json: @logs
+  end
+
+  private
+  def run_action(action)
+    if @fluentd.agent.public_send(action)
+      flash[:success] = t("messages.fluentd_start_stop_delay_notice", action: t("fluentd.common.#{action}"))
+    else
+      flash[:error] = t("messages.fluentd_#{action}_failed", brand: fluentd_ui_title)
+      flash[:error] += yield if block_given?
+    end
   end
 end
