6615dce3cefb35fd676d70dc3935746db8ad9927
uu59 <k@uu59.org>
2015-01-28 12:02:21 +0900

    Aggregate codemirror related code into codemirror.js

--- app/assets/javascripts/codemirror.js
+++ app/assets/javascripts/codemirror.js
@@ -12,3 +12,19 @@ $(function(){
     codemirrorify(el);
   });
 });
+
+Vue.directive('config-editor', {
+  bind: function(){
+    debugger;
+    var $parent = this.vm;
+    // NOTE: needed delay for waiting CodeMirror setup
+    _.delay(function(textarea){
+      var cm = codemirrorify(textarea);
+      // textarea.codemirror = cm; // for test, but doesn't work for now (working on Chrome, but Poltergeist not)
+      cm.on('change', function(code_mirror){
+        // bridge Vue - CodeMirror world
+        $parent.editContent = code_mirror.getValue();
+      });
+    }, 0, this.el);
+  }
+});
--- app/assets/javascripts/vue_common.js
+++ app/assets/javascripts/vue_common.js
@@ -2,17 +2,3 @@ Vue.filter('to_json', function (value) {
     return JSON.stringify(value);
 })
 
-
-Vue.directive('config-editor', {
-  bind: function(){
-    var $parent = this.vm;
-    // NOTE: needed delay for waiting CodeMirror setup
-    _.delay(function(textarea){
-      var cm = codemirrorify(textarea);
-      cm.on('change', function(code_mirror){
-        // bridge Vue - CodeMirror world
-        $parent.editContent = code_mirror.getValue();
-      });
-    }, 0, this.el);
-  }
-});
