5dcd1fdf461a0cb5d99def4c73bd784eb1818676
uu59 <k@uu59.org>
2015-02-04 16:23:06 +0900

    more human friendly name

--- app/models/fluentd/agent/local_common.rb
+++ app/models/fluentd/agent/local_common.rb
@@ -82,9 +82,9 @@ class Fluentd
 
       private
 
-      def exec_dryrun(exe, file_path = nil)
+      def exec_dryrun(command, file_path = nil)
         Bundler.with_clean_env do
-          system("#{exe} -q --dry-run #{options_to_argv(config_file: file_path)}", out: File::NULL, err: File::NULL)
+          system("#{command} -q --dry-run #{options_to_argv(config_file: file_path)}", out: File::NULL, err: File::NULL)
           raise ::Fluentd::Agent::ConfigError, last_error_message unless $?.exitstatus.zero?
         end
       end
