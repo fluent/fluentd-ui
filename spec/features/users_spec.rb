b94d362125604249790d5ea344401be4d18cfea9
yoshihara <h.yoshihara@everyleaf.com>
2015-04-08 13:54:50 +0900

    Wrap the unlogined spec with new description
    To distingish specs for logined user with the less indent.

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,9 +1,11 @@
 require "spec_helper"
 
 describe "users" do
-  describe "edit" do
+  describe "unlogined" do
     let(:url) { user_path }
     it_should_behave_like "login required"
   end
 
+  describe "edit" do
+  end
 end
