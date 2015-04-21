6158e4db8fca47c3c8067f71431b308fdef97bfe
yoshihara <h.yoshihara@everyleaf.com>
2015-04-21 15:26:49 +0900

    Remove needless accessor definition

--- lib/regexp_preview.rb
+++ lib/regexp_preview.rb
@@ -6,8 +6,6 @@ require "regexp_preview/single_line"
 require "regexp_preview/multi_line"
 
 module RegexpPreview
-  attr_reader :file, :format, :params, :time_format, :regexp
-
   def self.processor(format)
     case format
     when "multiline"
