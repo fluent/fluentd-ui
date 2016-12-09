33656e29f4624e2304254bb94405f8419b4c9a7c
uu59 <k@uu59.org>
2016-12-09 16:18:28 +0900

    Fix #208 for multiline preview

--- lib/regexp_preview/multi_line.rb
+++ lib/regexp_preview/multi_line.rb
@@ -27,24 +27,34 @@ module RegexpPreview
       reader = FileReverseReader.new(File.open(file))
       result = []
       target_lines = reader.tail(Settings.in_tail_preview_line_count).map{|line| line << "\n" }
-      target_lines.each_with_index do |line, line_no|
-        if line.match(params[:format_firstline])
-          lines = target_lines[line_no, patterns.length]
-          next if lines.length < patterns.length
-          ret = detect_chunk(lines)
-          next unless ret
-          result << ret
-        end
+      whole_string = target_lines.join
+      re_firstline = Regexp.new(params[:format_firstline])
+      indexes = []
+      cur = 0
+      while first_index = whole_string.index(re_firstline, cur)
+        indexes << first_index
+        cur = first_index + 1
+      end
+      indexes.each_with_index do |index, i|
+        next_index = indexes[i + 1] || -1
+        chunk = whole_string[index...next_index]
+        ret = detect_chunk(chunk)
+        next unless ret
+        result << ret
       end
       result
     end
 
-    def detect_chunk(lines)
+    def detect_chunk(chunk)
       whole = ""
       matches = []
-      lines.each_with_index do |line, i|
-        match = line.match(patterns[i])
+      offset = 0
+      patterns.each do |pat|
+        match = chunk.match(pat)
         return nil unless match
+        offset = chunk.index(pat)
+        return nil if offset > 0
+        chunk = chunk[match[0].length..-1]
         match.names.each_with_index do |name, index|
           matches << {
             key: name,
@@ -52,7 +62,7 @@ module RegexpPreview
             pos: match.offset(index + 1).map{|pos| pos + whole.length},
           }
         end
-        whole << line
+        whole << match[0]
       end
       {
         whole: whole,
@@ -63,7 +73,7 @@ module RegexpPreview
     def patterns
       @patterns ||= (1..20).map do |n|
         params["format#{n}"].presence
-      end.compact.map {|pattern| Regexp.new(pattern)}
+      end.compact.map {|pattern| Regexp.new(pattern, Regexp::MULTILINE)}
     end
   end
 end
