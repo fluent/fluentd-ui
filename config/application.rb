4fb68236694ed2f52eb65b0cde0a1c15a3ae3f66
Kenji Okimoto <okimoto@clear-code.com>
2018-04-18 17:12:22 +0900

    Set available locales to avoid errors
    > I18n::InvalidLocale: "ja" is not a valid locale
    
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,7 @@ module FluentdUi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = 'en'
+    config.i18n.available_locales = %i(en ja)
     config.autoload_paths += %W(#{config.root}/lib)
 
     config.active_job.queue_adapter = :sucker_punch
