b897df4e29c77d9174b4b011e9d04359cadf8979
Kenji Okimoto <okimoto@clear-code.com>
2018-04-24 10:56:38 +0900

    Remove outdated comment
    td-agent3's /etc/init.d/td-agent has reload command.
    
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

--- app/models/fluentd/agent/td_agent/unix.rb
+++ app/models/fluentd/agent/td_agent/unix.rb
@@ -13,8 +13,6 @@ class Fluentd
         end
 
         def restart
-          # NOTE: td-agent has no reload command
-          # https://github.com/treasure-data/td-agent/blob/master/debian/td-agent.init#L156
           detached_command('/etc/init.d/td-agent restart')
         end
       end
