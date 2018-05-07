7f0c921e1d4552846e313a74aeaff5ef89f15d97
Kenji Okimoto <okimoto@clear-code.com>
2018-05-07 15:31:48 +0900

    Use new style definition
    But I'm not sure that this change will work or not...
    
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

--- app/models/fluentd/setting/out_forward.rb
+++ app/models/fluentd/setting/out_forward.rb
@@ -1,70 +1,9 @@
 class Fluentd
   module Setting
     class OutForward
-      class Server
-        include Common
-        KEYS = [
-          :name, :host, :port, :weight, :standby
-        ].freeze
+      include Fluentd::Setting::Plugin
 
-        attr_accessor(*KEYS)
-
-        flags :standby
-
-        validates :host, presence: true
-        validates :port, presence: true
-      end
-
-      class Secondary
-        include Common
-        KEYS = [
-          :type, :path
-        ].freeze
-
-        attr_accessor(*KEYS)
-
-        hidden :type
-        validates :path, presence: true
-      end
-
-      include Common
-
-      KEYS = [
-        :match,
-        :send_timeout, :recover_wait, :heartbeat_type, :heartbeat_interval,
-        :phi_threshold, :hard_timeout,
-        :server, :secondary
-      ].freeze
-
-      attr_accessor(*KEYS)
-      choice :heartbeat_type, %w(udp tcp)
-      nested :server, Server, multiple: true
-      nested :secondary, Secondary
-
-      validates :match, presence: true
-      validate :validate_has_at_least_one_server
-      validate :validate_nested_values
-
-      def validate_has_at_least_one_server
-        if children_of(:server).reject{|s| s.empty_value? }.blank?
-          errors.add(:base, :out_forward_blank_server)
-        end
-      end
-
-      def validate_nested_values
-        self.class.children.inject(true) do |result, (key, _)|
-          children_of(key).each do |child|
-            if !child.empty_value? && !child.valid?
-              child.errors.full_messages.each do |message|
-                errors.add(:base, "(#{key})#{message}")
-              end
-              result = false
-            end
-            result
-          end
-          result
-        end
-      end
+      register_plugin("output", "forward")
 
       def self.initial_params
         {
