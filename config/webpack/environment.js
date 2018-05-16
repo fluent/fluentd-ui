5da5fa22eddcc0d26b628e5a8d68e5fda76c6ad3
Kenji Okimoto <okimoto@clear-code.com>
2018-05-16 13:39:29 +0900

    Add webpack plugins jQuery and Popper.js
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,20 @@
 const { environment } = require('@rails/webpacker')
 const vue =  require('./loaders/vue')
+const webpack = require('webpack');
+
+// Get a pre-configured plugin
+const manifestPlugin = environment.plugins.get('Manifest')
+manifestPlugin.opts.writeToFileEmit = false
+
+// Add an additional plugin of your choosing : ProvidePlugin
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+      $: 'jquery/dist/jquery',
+      jQuery: 'jquery/dist/jquery',
+      Popper: 'popper.js/dist/popper'
+  })
+);
 
 environment.loaders.append('vue', vue)
 module.exports = environment
