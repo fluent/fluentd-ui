0f9e417865b755b6e2f42e44b945ca901ac5bd9a
Kenji Okimoto <okimoto@clear-code.com>
2018-05-16 15:26:24 +0900

    Use rails-ujs
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,14 +10,4 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require startbootstrap-sb-admin/js/sb-admin
-//= require startbootstrap-sb-admin/js/sb-admin-datatables
-//= require vue/dist/vue.esm
-//= require es6-promise/dist/es6-promise
-//= require lodash/lodash
-//= require codemirror/lib/codemirror
-//= require codemirror_fluentd
-//= require vue_common
-//= require_tree .
+//= require rails-ujs
