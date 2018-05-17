dea13ddb16e95eaf69116868b54bc4b218cfce52
Kenji Okimoto <okimoto@clear-code.com>
2018-05-17 09:58:54 +0900

    Move fluent_log.js under app/javascript/packs
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

---
+++ app/javascript/packs/fluent_log.js
@@ -0,0 +1,58 @@
+'use strict';
+$(document).ready(()=> {
+  new Vue({
+    el: "#fluent-log",
+    paramAttributes: ["logUrl", "initialAutoReload"],
+    data: {
+      "autoFetch": false,
+      "logs": [],
+      "limit": 30,
+      "processing": false
+    },
+
+    compiled: function(){
+      this.fetchLogs();
+
+      var self = this;
+      var timer;
+      this.$watch("autoFetch", function(newValue){
+        if(newValue === true) {
+          timer = setInterval(function(){
+            self.fetchLogs();
+            var $log = $(".log", self.$el);
+            $log.scrollTop($log.innerHeight());
+          }, 1000);
+        } else {
+          clearInterval(timer);
+        }
+      });
+      if(this.initialAutoReload) {
+        this.autoFetch = true;
+      }
+    },
+
+    computed: {
+      isPresentedLogs: function(){
+        return this.logs.length > 0;
+      }
+    },
+
+    methods: {
+      fetchLogs: function() {
+        if(this.processing) return;
+        this.processing = true;
+        var self = this;
+        new Promise(function(resolve, reject) {
+          $.getJSON(self.logUrl + "?limit=" + self.limit, resolve).fail(reject);
+        }).then(function(logs){
+          self.logs = logs;
+          setTimeout(function(){
+            self.processing = false;
+          }, 256); // delay to reduce flicking loading icon
+        })["catch"](function(error){
+          self.processing = false;
+        });
+      }
+    }
+  });
+});
