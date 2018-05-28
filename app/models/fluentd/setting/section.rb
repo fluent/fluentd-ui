58f5ab3b102f989a958aded6ad786ef160e84a6b
Kenji Okimoto <okimoto@clear-code.com>
2018-05-28 14:58:02 +0900

    Construct section class properly
    In previous version, run merged block before calling init.
    
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

--- app/models/fluentd/setting/section.rb
+++ app/models/fluentd/setting/section.rb
@@ -10,19 +10,23 @@ class Fluentd
             include Fluentd::Setting::SectionParser
             include Fluentd::Setting::PluginParameter
 
-            class_attribute :_klass, :_block
+            class_attribute :_klass, :_block, :_blocks
             class_attribute :section_name, :required, :multi, :alias
             self._klass = klass
+            self._blocks = []
           end
         end
 
         def init
           _klass.instance_eval(&_block)
+          _blocks.each do |b|
+            _klass.instance_eval(&b)
+          end
         end
 
         # Don't overwrite options
-        def merge(**options)
-          _klass.instance_eval(&_block)
+        def merge(**options, &block)
+          _blocks << block
         end
 
         def section?
