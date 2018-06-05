c675753db4a962565fc140f913ecbb566d1cb236
Kenji Okimoto <okimoto@clear-code.com>
2018-06-05 15:28:02 +0900

    Set default values to in_monitor_agent
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

--- app/models/fluentd/setting/in_monitor_agent.rb
+++ app/models/fluentd/setting/in_monitor_agent.rb
@@ -9,12 +9,15 @@ class Fluentd
         {
           bind: "0.0.0.0",
           port: 24220,
+          emit_interval: 60,
+          include_config: true,
+          include_retry: true
         }
       end
 
       def common_options
         [
-          :bind, :port
+          :bind, :port, :tag
         ]
       end
     end
