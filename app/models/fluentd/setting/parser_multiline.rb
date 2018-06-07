20613928c5716edc9007b760b0e090cea470719d
Kenji Okimoto <okimoto@clear-code.com>
2018-06-07 17:36:44 +0900

    Define options for parser_multiline
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

--- app/models/fluentd/setting/parser_multiline.rb
+++ app/models/fluentd/setting/parser_multiline.rb
@@ -5,8 +5,19 @@ class Fluentd
 
       register_plugin("parser", "multiline")
 
+      FORMAT_MAX_NUM = 20
+
+      (1..FORMAT_MAX_NUM).each do |n|
+        config_param("format#{n}", :string)
+      end
+
       def self.initial_params
         {}
+      end
+
+      def common_options
+        [:format_firstline] +
+          (1..FORMAT_MAX_NUM).to_a.map{|n| "format#{n}".to_sym }
       end
     end
   end
