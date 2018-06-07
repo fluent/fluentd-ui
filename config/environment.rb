6128141918b4039845f8d737e2eaf748854effee
Kenji Okimoto <okimoto@clear-code.com>
2018-06-07 16:38:24 +0900

    Load Fluentd plugins
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative 'application'
 
+# Load fluentd libraries & plugins
+require "fluent/load"
+
 # Initialize the Rails application.
 Rails.application.initialize!
