1145123296da05d4fd8fdd98fe75d4683c9b1270
Kenji Okimoto <okimoto@clear-code.com>
2018-06-18 14:37:16 +0900

    Remove unnecessary code
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

--- app/controllers/fluentd/settings/out_tdlog_controller.rb
+++ app/controllers/fluentd/settings/out_tdlog_controller.rb
@@ -6,13 +6,4 @@ class Fluentd::Settings::OutTdlogController < ApplicationController
   def target_class
     Fluentd::Setting::OutTdlog
   end
-
-  def initial_params
-    {
-      buffer_type: "file",
-      buffer_path: "/var/log/td-agent/buffer/td",
-      auto_create_table: true,
-      match: "td.*.*",
-    }
-  end
 end
