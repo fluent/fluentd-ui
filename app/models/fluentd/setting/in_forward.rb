ee8c45db8ad33f17db3b650914743fd2a0b6e3d3
Kenji Okimoto <okimoto@clear-code.com>
2018-06-19 18:05:24 +0900

    Add transport_common_options,transport_advanced_options
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

--- app/models/fluentd/setting/in_forward.rb
+++ app/models/fluentd/setting/in_forward.rb
@@ -29,6 +29,35 @@ class Fluentd
           :blocking_timeout,
         ]
       end
+
+      def transport_common_options
+        [
+          :ca_path,
+          :cert_path,
+          :private_key_path,
+          :private_key_passphrase
+        ]
+      end
+
+      def transport_advanced_options
+        [
+          :version,
+          :ciphers,
+          :insecure,
+          :client_cert_auth,
+          :ca_cert_path,
+          :ca_private_key_path,
+          :ca_private_key_passphrase,
+          :generate_private_key_length,
+          :generate_cert_country,
+          :generate_cert_state,
+          :generate_cert_locality,
+          :generate_cert_common_name,
+          :generate_cert_common_name,
+          :generate_cert_expiration,
+          :generate_cert_digest
+        ]
+      end
     end
   end
 end
