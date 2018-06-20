d6516ea172a4f02cca71ca9f15b19199097192c6
Kenji Okimoto <okimoto@clear-code.com>
2018-06-20 15:18:32 +0900

    Convert to vue component
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

--- app/javascript/packs/transport_config.js
+++ app/javascript/packs/transport_config.js
@@ -2,66 +2,59 @@
 
 import ConfigField from './config_field'
 
-$(document).ready(() => {
-  new Vue({
-    el: '#transport-config',
-    components: {
-      "config-field": ConfigField
-    },
-    props: [
-      "transportType",
-    ],
-    propsData: {
-      "transportType": "tcp"
-    },
-    data: function() {
-      return {
-        pluginType: null,
-        pluginName: null,
-        options: ["tcp", "tls"],
-        tlsOptions: []
-      }
-    },
-    computed: {
-      token: function() {
-        return Rails.csrfToken()
-      }
-    },
-    filters: {
-      toUpper: function(value) {
-        return _.toUpper(value)
-      }
-    },
-    beforeMount: function() {
-      this.pluginType = this.$el.attributes.pluginType.nodeValue
-      this.pluginName = this.$el.attributes.pluginName.nodeValue
-    },
-    mounted: function() {
-    },
-    methods: {
-      onChange: function() {
-        console.log(this.pluginType, this.pluginName, this.transportType)
-        this.updateSection()
-      },
-
-      updateSection: function() {
-        if (this.transportType === "tcp") {
-          return
-        }
-        $.ajax({
-          method: "GET",
-          url: "/api/config_definitions",
-          headers: {
-            'X-CSRF-Token': this.token
-          },
-          data: {
-            type: this.pluginType,
-            name: this.pluginName
-          }
-        }).then((data) => {
-          this.tlsOptions = data.tlsOptions
-        })
-      }
+const TransportConfig = {
+  template: "#vue-transport-config",
+  components: {
+    "config-field": ConfigField
+  },
+  props: [
+    "pluginType",
+    "pluginName"
+  ],
+  data: function() {
+    return {
+      transportType: "tcp",
+      options: ["tcp", "tls"],
+      tlsOptions: []
     }
-  })
-})
+  },
+  computed: {
+    token: function() {
+      return Rails.csrfToken()
+    }
+  },
+  filters: {
+    toUpper: function(value) {
+      return _.toUpper(value)
+    }
+  },
+  mounted: function() {
+  },
+  methods: {
+    onChange: function() {
+      console.log(this.pluginType, this.pluginName, this.transportType)
+      this.updateSection()
+    },
+
+    updateSection: function() {
+      if (this.transportType === "tcp") {
+        return
+      }
+      $.ajax({
+        method: "GET",
+        url: "/api/config_definitions",
+        headers: {
+          'X-CSRF-Token': this.token
+        },
+        data: {
+          type: this.pluginType,
+          name: this.pluginName
+        }
+      }).then((data) => {
+        this.tlsOptions = data.tlsOptions
+      })
+    }
+  }
+}
+
+export { TransportConfig as default }
