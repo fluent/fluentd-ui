2a01337ebd88f9b9178c3120325d7b048c1d17e3
Kenji Okimoto <okimoto@clear-code.com>
2018-06-21 15:30:13 +0900

    Enable JavaScript
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

--- spec/features/fluentd/setting/in_forward_spec.rb
+++ spec/features/fluentd/setting/in_forward_spec.rb
@@ -1,7 +1,6 @@
 require "spec_helper"
 
-describe "in_forward", stub: :daemon do
+describe "in_forward", stub: :daemon, js: true do
   before { login_with exists_user }
   it_should_behave_like "configurable daemon settings", "in_forward", "port", "12345"
-
 end
