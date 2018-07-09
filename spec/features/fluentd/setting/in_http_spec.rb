a570aa31810a522f15698459e4fa6dae678c62c4
Kenji Okimoto <okimoto@clear-code.com>
2018-07-09 16:25:48 +0900

    Use JavaScript
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

--- spec/features/fluentd/setting/in_http_spec.rb
+++ spec/features/fluentd/setting/in_http_spec.rb
@@ -1,7 +1,6 @@
 require "spec_helper"
 
-describe "in_http", stub: :daemon do
+describe "in_http", js: true, stub: :daemon do
   before { login_with exists_user }
   it_should_behave_like "configurable daemon settings", "in_http", "port", "12345"
-
 end
