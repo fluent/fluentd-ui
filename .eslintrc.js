abc3e3039cbe6c23e69acc2e977218e6313a12e8
Kenji Okimoto <okimoto@clear-code.com>
2018-07-18 12:11:47 +0900

    Add eslint
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2017,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
