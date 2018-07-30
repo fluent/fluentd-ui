4f950bbde6a46d6a20158be4cc0c924c5c43c972
Kenji Okimoto <okimoto@clear-code.com>
2018-07-30 18:30:32 +0900

    Remove needless configuration
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,20 +23,6 @@ class ActiveSupport::TestCase
   extend FixturePath
 end
 
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app,
-    browser: :chrome,
-    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-      chrome_options: {
-        args: %w(headless disable-gpu window-size=1920,1080),
-      },
-    )
-  )
-end
-
-Capybara.javascript_driver = :selenium
-require "capybara-screenshot/testunit"
-
 Dir[Rails.root.join("test/support/**/*.rb")].each do |path|
   require(path)
 end
