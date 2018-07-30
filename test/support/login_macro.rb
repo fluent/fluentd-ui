d617f9f661b8488790f895254e6673dafb2328f0
Kenji Okimoto <okimoto@clear-code.com>
2018-07-30 14:02:55 +0900

    Add test/integration/sesstions_test.rb
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

--- test/support/login_macro.rb
+++ test/support/login_macro.rb
@@ -1,9 +1,9 @@
 module LoginMacro
-  def login_with(user)
+  def login_with(user, password: nil)
     visit("/sessions/new")
     within("form") do
       fill_in("session_name", :with => user.name)
-      fill_in("session_password", :with => user.password)
+      fill_in("session_password", :with => password || user.password)
     end
     click_button(I18n.t("terms.sign_in"))
   end
