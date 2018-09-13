881b357c77fa8abed41deecd4b3ac1d06ec274b6
Kenji Okimoto <okimoto@clear-code.com>
2018-09-13 16:35:24 +0900

    Suppress FactoryBot warnings
    See also https://robots.thoughtbot.com/deprecating-static-attributes-in-factory_bot-4-11
    
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

--- test/factories/fluentd.rb
+++ test/factories/fluentd.rb
@@ -3,9 +3,9 @@ FactoryBot.define do
     dir = Rails.root.join("tmp/fluentd-test").to_s
     FileUtils.mkdir_p(dir)
 
-    variant "fluentd_gem"
-    log_file dir + "/fluentd.log"
-    pid_file dir + "/fluentd.pid"
-    config_file dir + "/fluentd.conf"
+    variant { "fluentd_gem" }
+    log_file { dir + "/fluentd.log" }
+    pid_file { dir + "/fluentd.pid" }
+    config_file { dir + "/fluentd.conf" }
   end
 end
--- test/factories/plugins.rb
+++ test/factories/plugins.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :plugin do
-    gem_name "fluent-plugin-dummy"
-    version "1.2.3"
+    gem_name { "fluent-plugin-dummy" }
+    version { "1.2.3" }
   end
 end
