76f8c3715070157b8333dbbbbb83f14a0586062f
Kenji Okimoto <okimoto@clear-code.com>
2018-09-28 09:53:42 +0900

    Add label parameter to owner plugins
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

--- app/models/concerns/fluentd/setting/plugin.rb
+++ app/models/concerns/fluentd/setting/plugin.rb
@@ -18,7 +18,6 @@ class Fluentd
       include Fluentd::Setting::PluginConfig
       include Fluentd::Setting::SectionParser
       include Fluentd::Setting::PluginParameter
-      include Fluentd::Setting::Label
 
       included do
         cattr_accessor :plugin_type, :plugin_name, :config_definition
@@ -31,6 +30,10 @@ class Fluentd
 
           if ["filter", "output"].include?(type)
             include Fluentd::Setting::Pattern
+          end
+
+          if ["input", "filter", "output"].include?(type)
+            include Fluentd::Setting::Label
           end
 
           self.load_plugin_config do |_name, params|
