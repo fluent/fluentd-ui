19bd3d06c17f8047707dfec5f1e0adc3213f2b76
Kenji Okimoto <okimoto@clear-code.com>
2018-09-28 11:59:23 +0900

    Add parser_plugin_form component
    This is copied from owned_plugin_form.
    
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

--- app/javascript/packs/in_tail_parse.js
+++ app/javascript/packs/in_tail_parse.js
@@ -3,13 +3,13 @@
 import "lodash/lodash";
 import "popper.js/dist/popper";
 import "bootstrap/dist/js/bootstrap";
-import OwnedPluginForm from "./owned_plugin_form";
+import ParserPluginForm from "./parser_plugin_form";
 
 $(document).ready(() => {
   new Vue({
     el: "#in-tail-parse",
     components: {
-      "owned-plugin-form": OwnedPluginForm
+      "parser-plugin-form": ParserPluginForm
     },
     data: function() {
       return {
