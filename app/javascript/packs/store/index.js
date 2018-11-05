202200b9f61fd4b1764ce63bc3a6020a45e8ba6e
Kenji Okimoto <okimoto@clear-code.com>
2018-11-05 11:09:57 +0900

    Use double quotes instead of single quotes
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

--- app/javascript/packs/store/index.js
+++ app/javascript/packs/store/index.js
@@ -5,7 +5,7 @@ import createLogger from "vuex/dist/logger";
 
 Vue.use(Vuex);
 
-const debug = process.env.NODE_ENV !== 'production';
+const debug = process.env.NODE_ENV !== "production";
 
 import parserParams from "./modules/parser_params";
 
